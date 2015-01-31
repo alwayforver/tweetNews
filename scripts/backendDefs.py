c37275043a09fdf16e0842b283ae01f75ce10a3a
alwayforver <wangjj.smile@gmail.com>
2015-01-31 16:20:54 -0600

    dtpure

--- scripts/backendDefs.py
+++ scripts/backendDefs.py
@@ -1,6 +1,6 @@
 from datetime import datetime as dt
 class News:
-    def __init__(self,ID,title,raw_text,snippets,key_word,source,created_at):
+    def __init__(self,ID,title,raw_text,snippets,key_word,source,created_at,dtpure):
         self.ID = ID
         self.title = title
         self.raw_text = raw_text
@@ -8,6 +8,7 @@ class News:
         self.key_word = key_word
         self.source = source
         self.created_at = dt.strptime(created_at[5:-6],"%d %b %Y %H:%M:%S")
+        self.dtpure = dtpure
     local_time_zone = None
     url = None
     def _tz(self, tz):    
@@ -26,7 +27,11 @@ class Tweet:
     def __init__(self,ID,raw_text,created_at,is_retweet,retweet_count,hash_tags):
         self.ID = ID
         self.raw_text = raw_text
-        self.created_at = created_at
+        try: 
+            self.created_at = dt.strptime(created_at[4:-11] + " " +created_at[-4:],"%b %d %H:%M:%S %Y")
+        except:
+            print("time error")
+            self.created_at = dt.today()
         self.is_retweet = is_retweet
         self.retweet_count = retweet_count
         self.hash_tags = hash_tags
